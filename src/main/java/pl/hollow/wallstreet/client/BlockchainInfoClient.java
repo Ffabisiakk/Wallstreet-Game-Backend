f89178bd0784afa6829f8fba870870dca9e6662f
Ffabisiakk <ffabisiakk@gmail.com>
2020-01-21 14:51:40 +0100

    BlockchainInfoClient implemented

---
+++ src/main/java/pl/hollow/wallstreet/client/BlockchainInfoClient.java
@@ -0,0 +1,25 @@
+package pl.hollow.wallstreet.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import pl.hollow.wallstreet.util.StringUtil;
+
+import java.math.BigDecimal;
+
+@Component
+public class BlockchainInfoClient {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public BlockchainInfoClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public BigDecimal getBitcoinCurrencyRate(String currency, String amount) {
+        return restTemplate.getForObject(StringUtil
+                .format("https://blockchain.info/tobtc?{}&{}", currency, amount),
+                BigDecimal.class);
+    }
+}
