1485af5ac9af4a8814ffd5dce69c77944e4f9998
Ffabisiakk <ffabisiakk@gmail.com>
2020-02-03 14:42:08 +0100

    Clients tests

---
+++ src/test/java/pl/hollow/wallstreet/client/blockchaninfo/BlockchainInfoClientTest.java
@@ -0,0 +1,33 @@
+package pl.hollow.wallstreet.client.blockchaninfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@SpringBootTest
+class BlockchainInfoClientTest {
+
+    @Autowired
+    private BlockchainInfoClient client;
+
+    @Test
+    public void shouldFetchFlag() {
+//        When
+        Double result = client.getBitcoinCurrencyRate("PLN", "test");
+
+//        Then
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void shouldFetchBitcoinRates() {
+//        When
+        Double result = client.getBitcoinCurrencyRate("PLN", "1");
+
+//        Then
+        assertNotEquals(-1, result);
+    }
+}
