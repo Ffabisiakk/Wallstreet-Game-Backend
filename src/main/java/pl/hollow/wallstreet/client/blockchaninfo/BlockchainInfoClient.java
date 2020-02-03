450a28f9e32ebc1953386c7bc9802d028606b23f
Ffabisiakk <ffabisiakk@gmail.com>
2020-02-03 14:41:34 +0100

    Client refactor

--- src/main/java/pl/hollow/wallstreet/client/blockchaninfo/BlockchainInfoClient.java
+++ src/main/java/pl/hollow/wallstreet/client/blockchaninfo/BlockchainInfoClient.java
@@ -1,12 +1,17 @@
 package pl.hollow.wallstreet.client.blockchaninfo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import pl.hollow.wallstreet.util.StringUtil;
 
 @Component
 public class BlockchainInfoClient {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(BlockchainInfoClient.class);
 
     private RestTemplate restTemplate;
 
@@ -15,8 +20,17 @@ public class BlockchainInfoClient {
         this.restTemplate = restTemplate;
     }
 
-    public String getBitcoinCurrencyRate(String currency, String amount) {
+    public Double getBitcoinCurrencyRate(String currency, String amount) {
         String uri = StringUtil.format("https://blockchain.info/tobtc?currency={}&value={}", currency, amount);
-        return restTemplate.getForObject(uri, String.class);
+        try {
+            String result = restTemplate.getForObject(uri, String.class);
+            return Double.parseDouble(result.replace(",", ""));
+        } catch (RestClientException e) {
+            LOGGER.warn("Could't fetch Bitcoin rate.");
+            return -1.0;
+        } catch (NullPointerException e) {
+            LOGGER.warn("Empty result set.");
+            return -1.0;
+        }
     }
 }
