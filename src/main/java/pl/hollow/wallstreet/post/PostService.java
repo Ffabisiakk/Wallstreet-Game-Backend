25f37072316d158fe1b63c627605295225aa7883
Ffabisiakk <ffabisiakk@gmail.com>
2020-02-05 10:58:33 +0100

    Post service

---
+++ src/main/java/pl/hollow/wallstreet/post/PostService.java
@@ -0,0 +1,47 @@
+package pl.hollow.wallstreet.post;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.hollow.wallstreet.exception.EntityNotFoundException;
+
+import java.util.List;
+
+@Service
+class PostService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PostService.class);
+
+    private PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Autowired
+    public List<Post> getPosts() {
+        LOGGER.info("Fetching list of all posts.");
+        return postRepository.findAll();
+    }
+
+    public Post getPost(String id) throws EntityNotFoundException {
+        LOGGER.info("Fetching post {}", id);
+        return postRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Post: " + id));
+    }
+
+    public void createPost(Post post) {
+        LOGGER.info("Creating post {}", post.getId());
+        postRepository.save(post);
+    }
+
+    public Post updatePost(Post post) {
+        LOGGER.info("Updating post {}", post.getId());
+        return postRepository.save(post);
+    }
+
+    public void deletePost(String id) {
+        LOGGER.info("Deleting post {}", id);
+        postRepository.deleteById(id);
+    }
+}
