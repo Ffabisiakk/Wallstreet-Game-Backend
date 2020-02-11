22a89cfa1936aa92fd2cedfab57adebb07830d2f
Ffabisiakk <ffabisiakk@gmail.com>
2020-02-11 14:47:08 +0100

    Post refactor

--- src/main/java/pl/hollow/wallstreet/post/PostMapper.java
+++ src/main/java/pl/hollow/wallstreet/post/PostMapper.java
@@ -5,14 +5,10 @@ import org.mapstruct.Mapper;
 import org.springframework.data.domain.Page;
 import pl.hollow.wallstreet.post.dto.PostDto;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public abstract class PostMapper {
 
     abstract PostDto postToPostDto(Post post);
-    abstract Post postDtoToPost(PostDto postDto);
-    abstract List<PostDto> postsToPostDtos(List<Post> posts);
     public Page<PostDto> postPageToPostDtoPage(Page<Post> postPage) {
         return postPage.map(this::postToPostDto);
     }
